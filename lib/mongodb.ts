ab881b135324dd21898c5d3aa64a3b28ff254f84
Ariel Leyva <aleyva1509@gmail.com>
2024-01-25 14:36:46 -0600

    add: mongodb setup

---
+++ lib/mongodb.ts
@@ -0,0 +1,47 @@
+// lib/mongodb.ts
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+type Cached = {
+    conn: mongoose.Connection | null,
+    promise: Promise<mongoose.Connection> | null
+};
+
+declare global {
+    var mongoose: Cached;
+}
+
+async function dbConnect(): Promise<mongoose.Connection> {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts: mongoose.ConnectOptions = {
+        };
+
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+        return mongoose.connection;
+        });
+    }
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
+
+export default dbConnect;
