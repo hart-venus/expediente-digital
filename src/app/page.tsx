cbc8a6391eb37824fac9d6a69c37b5f836c27117
Ariel Leyva <aleyva1509@gmail.com>
2024-01-25 04:28:02 -0600

    better search icon and separator line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <h1 className={styles.header}>Expediente Digital</h1>
       <SearchBar/>
+      <div className={styles.divBar}/>
     </main>
   );
 }
