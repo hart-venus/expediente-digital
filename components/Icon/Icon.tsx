3a7013120de3fcefb6df6d01949a32f43aed859d
Ariel Leyva <aleyva1509@gmail.com>
2024-01-25 04:06:30 -0600

    add: icon for search

--- components/Icon/Icon.tsx
+++ components/Icon/Icon.tsx
@@ -1,12 +1,14 @@
+'use client';
 import { Icon } from '@iconify/react';
 import styles from './Icon.module.css';
 
 interface IconProps {
     icon: string;
+    className?: string;
 }
 
 export default function IconComponent(props: IconProps) {
     return (
-        <Icon icon={props.icon} className={styles.icon}/>
+        <Icon icon={props.icon} className={`${styles.icon} ${props.className}`}/>
     );
 }
--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,9 +1,11 @@
 import styles from "./SearchBar.module.css";
+import Icon from "../Icon/Icon";
 
 export default function SearchBar() {
     return (
-        <>
+        <main className={styles.main}>
             <input type="text" placeholder="Buscar pacientes..." className={styles.searchBar}/>
-        </>
+            <Icon icon="material-symbols:search" className={styles.searchIcon}/>
+        </main>
     );
 }
