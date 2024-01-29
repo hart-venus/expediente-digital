eb1e339d26b1813949beeadf3085c8f0984f5e60
Ariel Leyva <aleyva1509@gmail.com>
2024-01-28 22:10:58 -0600

    add grid types and dependency

--- models/Pdf.ts
+++ models/Pdf.ts
@@ -1,4 +1,6 @@
 import mongoose, { Document, Schema } from 'mongoose';
+//gridfs-stream module to read and write files to MongoDB
+import Grid from 'gridfs-stream';
 
 interface IPdf extends Document {
     
