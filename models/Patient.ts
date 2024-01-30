f1d24b197a3f15cde81d844e8268a1ae5a8862ee
Ariel Leyva <aleyva1509@gmail.com>
2024-01-30 16:19:41 -0600

    finish patient interface

--- models/Patient.ts
+++ models/Patient.ts
@@ -3,6 +3,15 @@ import mongoose, { Document, Schema } from 'mongoose';
 interface IPatient extends Document {
     fullName: string;
     governmentId: string;
-    
-
+    birthDate: Date;
+    phoneNumber: string;
+    email: string;
+    familyBackground: string;
+    pathologicBackground: string;
+    nonPathologicBackground: string;
+    chirurgicalBackground: string;
+    ginecoObstetricBackground: string;
+    diagnosis: string;
+    treatment: string;
+    examPdfPath: string; // reference to the pdf in the gridfs
 }
--- models/Pdf.ts
+++
@@ -1,6 +0,0 @@
-import mongoose, { Document, Schema } from 'mongoose';
-//gridfs-stream module to read and write files to MongoDB
-import Grid from 'gridfs-stream';
-
-interface IPdf extends Document {
-    
