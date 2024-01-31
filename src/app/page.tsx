3c8d4795c3130d87181bba2316602dd73cb47fcf
Ariel Leyva <aleyva1509@gmail.com>
2024-01-31 14:49:35 -0600

    add: new button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import styles from "./page.module.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import IconComponent from "../../components/Icon/Icon";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.header}>Expediente Digital</h1>
+      <div className={styles.navbar}> 
+        <h1 className={styles.header}>Expediente Digital</h1>
+        <Link href="/new">  
+          <IconComponent icon="fa:plus-square" className={styles.icon}/>
+        </Link>
+      </div>
       <SearchBar/>
       <div className={styles.divBar}/>
     </main>
