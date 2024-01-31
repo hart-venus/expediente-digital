ec29f48b341bc7922aa1c9c321914b5b173511d8
Ariel Leyva <aleyva1509@gmail.com>
2024-01-30 21:29:43 -0600

    add: zod validation and schema

---
+++ schema/patientSchema.ts
@@ -0,0 +1,44 @@
+// schemas/patientSchema.ts
+import { z } from 'zod';
+
+const patientSchema = z.object({
+    fullName: z.string()
+        .min(1, 'Se requiere un nombre completo')
+        .transform((str) => str.trim()),
+    governmentId: z.string()
+        .min(1, 'Se requiere un número de identificación')
+        .transform((str) => str.trim()),
+    birthDate: z.date(),
+    phoneNumber: z.string()
+        .optional()
+        .transform((str) => str ? str.trim() : str),
+    email: z.string()
+        .email('Por favor ingrese un correo electrónico válido')
+        .transform((str) => str.toLowerCase().trim()),
+    familyBackground: z.string()
+        .optional()
+        .transform((str) => str ? str.trim() : str),
+    pathologicBackground: z.string()
+        .optional()
+        .transform((str) => str ? str.trim() : str),
+    nonPathologicBackground: z.string()
+        .optional()
+        .transform((str) => str ? str.trim() : str),
+    chirurgicalBackground: z.string()
+        .optional()
+        .transform((str) => str ? str.trim() : str),
+    ginecoObstetricBackground: z.string()
+        .optional()
+        .transform((str) => str ? str.trim() : str),
+    diagnosis: z.string()
+        .optional()
+        .transform((str) => str ? str.trim() : str),
+    treatment: z.string()
+        .optional()
+        .transform((str) => str ? str.trim() : str),
+    examPdfPath: z.string()
+        .optional()
+        .transform((str) => str ? str.trim() : str)
+});
+
+export default patientSchema;
