9233d0c9539c9274dce5b7f5584642816ac7a3d8
Ariel Leyva <aleyva1509@gmail.com>
2024-02-01 14:18:56 -0600

    create form skeleton

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,8 +1,14 @@
+'use client';
 import styles from "./page.module.css";
 import Link from "next/link";
 import IconComponent from "../../../components/Icon/Icon";
+import { useState } from "react";
 
 export default function New() {
+    const [errors, setErrors] = useState({} as Record<string, string>);
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        console.log("submit");
+    }
     return (
         <main className={styles.main}>
             <div className={styles.navbar}> 
@@ -12,6 +18,9 @@ export default function New() {
                 <h1 className={styles.header}>Registrar Paciente</h1>
             </div>
             <div className={styles.divBar}/>
+            <form onSubmit={handleSubmit}>
+                
+            </form>
         </main>
     )
 }
