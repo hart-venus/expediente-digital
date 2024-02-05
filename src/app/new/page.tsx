bc093b150ec59bad66cc52653ec688e538528fd2
Ariel Leyva <aleyva1509@gmail.com>
2024-02-05 08:22:34 -0600

    add: error icon

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -99,23 +99,30 @@ export default function New() {
                     <div className={styles.formSection}>
                         <label htmlFor="fullName" className={styles.label}>Nombre *</label>
                         <input type="text" id="fullName" name="fullName" className={styles.input} required/>
-                        {errors.fullName && <p className={styles.error}>{errors.fullName}</p>}
-
+                        {errors.fullName && <p className={styles.error}>
+                            <IconComponent icon="icon-park-solid:error" className={styles.errorIcon}/>
+                            {errors.fullName}
+                        </p>}
                         <label htmlFor="governmentId" className={styles.label}>Cédula *</label>
                         <input type="text" id="governmentId" name="governmentId" className={styles.input} required/>
-                        {errors.governmentId && <p className={styles.error}>{errors.governmentId}</p>}
-                    
+                        {errors.governmentId && <p className={styles.error}>
+                            <IconComponent icon="icon-park-solid:error" className={styles.errorIcon}/>
+                            {errors.governmentId}</p>}
                         <label htmlFor="birthDate" className={styles.label}>Fecha de Nacimiento *</label>
                         <input type="date" id="birthDate" name="birthDate" className={styles.input} required/>
-                        {errors.birthDate && <p className={styles.error}>{errors.birthDate}</p>}
-
+                        {errors.birthDate && <p className={styles.error}>
+                            <IconComponent icon="icon-park-solid:error" className={styles.errorIcon}/>
+                            {errors.birthDate}</p>}
                         <label htmlFor="phoneNumber" className={styles.label}>Teléfono</label>
                         <input type="text" id="phoneNumber" name="phoneNumber" className={styles.input}/>
-                        {errors.phoneNumber && <p className={styles.error}>{errors.phoneNumber}</p>}
-
+                        {errors.phoneNumber && <p className={styles.error}>
+                            <IconComponent icon="icon-park-solid:error" className={styles.errorIcon}/>
+                            {errors.phoneNumber}</p>}
                         <label htmlFor="email" className={styles.label}>Correo Electrónico *</label>
                         <input type="email" id="email" name="email" className={styles.input} required/>
-                        {errors.email && <p className={styles.error}>{errors.email}</p>}
+                        {errors.email && <p className={styles.error}>
+                            <IconComponent icon="icon-park-solid:error" className={styles.errorIcon}/>
+                            {errors.email}</p>}
                     </div>
 
                     <div className={styles.userBox}>
@@ -128,19 +135,29 @@ export default function New() {
                 </div>
                 <label htmlFor="familyBackground" className={styles.label}>Antecedentes Familiares</label>
                 <textarea id="familyBackground" name="familyBackground" className={styles.textarea}/>
-                {errors.familyBackground && <p className={styles.error}>{errors.familyBackground}</p>}
+                {errors.familyBackground && <p className={styles.error}>
+                    <IconComponent icon="icon-park-solid:error" className={styles.errorIcon}/>
+                    {errors.familyBackground}</p>}
                 <label htmlFor="pathologicBackground" className={styles.label}>Antecedentes Patológicos</label>
                 <textarea id="pathologicBackground" name="pathologicBackground" className={styles.textarea}/>
-                {errors.pathologicBackground && <p className={styles.error}>{errors.pathologicBackground}</p>}
+                {errors.pathologicBackground && <p className={styles.error}>
+                    <IconComponent icon="icon-park-solid:error" className={styles.errorIcon}/>
+                    {errors.pathologicBackground}</p>}
                 <label htmlFor="nonPathologicBackground" className={styles.label}>Antecedentes No Patológicos</label>
                 <textarea id="nonPathologicBackground" name="nonPathologicBackground" className={styles.textarea}/>
-                {errors.nonPathologicBackground && <p className={styles.error}>{errors.nonPathologicBackground}</p>}
+                {errors.nonPathologicBackground && <p className={styles.error}>
+                    <IconComponent icon="icon-park-solid:error" className={styles.errorIcon}/>
+                    {errors.nonPathologicBackground}</p>}
                 <label htmlFor="chirurgicalBackground" className={styles.label}>Antecedentes Quirúrgicos</label>
                 <textarea id="chirurgicalBackground" name="chirurgicalBackground" className={styles.textarea}/>
-                {errors.chirurgicalBackground && <p className={styles.error}>{errors.chirurgicalBackground}</p>}
+                {errors.chirurgicalBackground && <p className={styles.error}>
+                    <IconComponent icon="icon-park-solid:error" className={styles.errorIcon}/>
+                    {errors.chirurgicalBackground}</p>}
                 <label htmlFor="ginoObstetricBackground" className={styles.label}>Antecedentes Gineco-Obstétricos</label>
                 <textarea id="ginoObstetricBackground" name="ginoObstetricBackground" className={styles.textarea}/>
-                {errors.ginoObstetricBackground && <p className={styles.error}>{errors.ginoObstetricBackground}</p>}
+                {errors.ginoObstetricBackground && <p className={styles.error}>
+                    <IconComponent icon="icon-park-solid:error" className={styles.errorIcon}/>
+                    {errors.ginoObstetricBackground}</p>}
                 <div className={styles.subheaderContainer}>
                     <h2 className={styles.subheader}>Evaluación</h2>
                     <div className={styles.divBar}/>
@@ -148,11 +165,14 @@ export default function New() {
 
                 <label htmlFor="diagnosis" className={styles.label}>Diagnóstico</label>
                 <textarea id="diagnosis" name="diagnosis" className={styles.textarea}/>
-                {errors.diagnosis && <p className={styles.error}>{errors.diagnosis}</p>}
+                {errors.diagnosis && <p className={styles.error}>
+                    <IconComponent icon="icon-park-solid:error" className={styles.errorIcon}/>
+                    {errors.diagnosis}</p>}
                 <label htmlFor="treatment" className={styles.label}>Tratamiento</label>
                 <textarea id="treatment" name="treatment" className={styles.textarea}/>
-                {errors.treatment && <p className={styles.error}>{errors.treatment}</p>}
-
+                {errors.treatment && <p className={styles.error}>
+                    <IconComponent icon="icon-park-solid:error" className={styles.errorIcon}/>
+                    {errors.treatment}</p>}
                 <div className={styles.buttonContainer}>
                     <div className={styles.fileModal}>
                         <input type="file" id="file" name="file" className={styles.fileInput} onChange={handleFileSelect}/>
@@ -166,7 +186,9 @@ export default function New() {
                     <button type="submit" className={styles.button}>Registrar</button>
                 </div>
 
-                {errors.nonFieldError && <p className={styles.nonFieldError}>{errors.nonFieldError}</p>}
+                {errors.nonFieldError && <p className={styles.nonFieldError}>
+                    <IconComponent icon="icon-park-solid:error" className={styles.errorIcon}/>
+                    {errors.nonFieldError}</p>}
             </form>
         </main>
     )
