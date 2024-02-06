9f544aa48ace1308fdc3ddeb3e00860ccd0c34d4
Ariel Leyva <aleyva1509@gmail.com>
2024-02-05 23:56:07 -0600

    feat: fetching works from frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,41 @@
+"use client";
 import styles from "./page.module.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import IconComponent from "../../components/Icon/Icon";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+interface patientInfo {
+  id: string;
+  fullName: string;
+  governmentId: string;
+  birthDate: string; // needs to be parsed into a Date object
+  phoneNumber: string; // will be returned as "" if not present
+  email: string;
+}
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [patients, setPatients] = useState<patientInfo[]>([]);
+
+  useEffect(() => {
+    console.log("Fetching patients");
+    setLoading(true);
+    fetch("http://localhost:3000/api/patients")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPatients(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <main className={styles.main}>
+    <main className={`${styles.main} ${loading ? styles.loading : ''}`}>
       <div className={styles.navbar}> 
         <h1 className={styles.header}>Expediente Digital</h1>
         <Link href="/new">  
