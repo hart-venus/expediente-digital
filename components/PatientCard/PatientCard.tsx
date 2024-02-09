f3fc275e8c3e18de0212a060021c6dba307b9384
Ariel Leyva <aleyva1509@gmail.com>
2024-02-09 14:05:15 -0600

    feat: add field icons and finish card styles

--- components/PatientCard/PatientCard.tsx
+++ components/PatientCard/PatientCard.tsx
@@ -23,10 +23,22 @@ export default function PatientCard(props: PatientCardProps) {
 
             <div className={styles.userInfo}>
                 <h2 className={styles.name}>{props.fullName}</h2>
-                <p className={styles.field}>{props.governmentId}</p>
-                <p className={styles.field}>{formattedDate}</p>
-                <p className={styles.field}>{props.phoneNumber ? props.phoneNumber : "Sin n. teléfono"}</p>
-                <p className={styles.field}>{props.email}</p>
+                <p className={styles.field}>
+                    <IconComponent icon="tabler:id-badge-2" className={styles.fieldIcon}/>
+                    {props.governmentId}
+                </p>
+                <p className={styles.field}>
+                    <IconComponent icon="mingcute:birthday-2-line" className={styles.fieldIcon}/>
+                    {formattedDate}
+                </p>
+                <p className={styles.field}>
+                    <IconComponent icon="mingcute:phone-line" className={styles.fieldIcon}/>
+                    {props.phoneNumber ? props.phoneNumber : "Sin n. teléfono"}
+                </p>
+                <p className={styles.field}>
+                    <IconComponent icon="jam:paper-plane" className={styles.fieldIcon}/>
+                    {props.email}
+                </p>
             </div>
         </div>
     );
