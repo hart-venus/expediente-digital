bfd372bab49fef6a88770d8e4610c9edfeee5b55
Ariel Leyva <aleyva1509@gmail.com>
2024-02-10 00:17:59 -0600

    hotfix: validate that a patient is active

--- src/app/api/patients/[id]/route.ts
+++ src/app/api/patients/[id]/route.ts
@@ -1,12 +1,19 @@
 import { NextResponse } from "next/server";
 import Patient from "../../../../../models/Patient";
+import { dbConnect } from "../../../../../lib/mongodb";
 
 export async function GET(req: Request, { params }: { params: { id: string } }) {
     // returns a patient's full information, and makes sure to return the file content if there's an examPdfId
+    await dbConnect();
     try {
         const patient = await Patient.findById(params.id); 
+        // if patient's isActive is false, throw
+        if (!patient?.isActive) {
+            throw new Error("Patient not found");
+        }
         return NextResponse.json(patient);
-    } catch {
+    } catch (e) {
+        console.log(e);
         return NextResponse.json({ error: "Patient not found" }, { status: 404 });
     }
 
