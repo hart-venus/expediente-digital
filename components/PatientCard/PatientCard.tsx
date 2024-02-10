57f37ff79673c94893b733b87246a075f206c08b
Ariel Leyva <aleyva1509@gmail.com>
2024-02-10 01:34:47 -0600

    fix: make patient card link to patient

--- components/PatientCard/PatientCard.tsx
+++ components/PatientCard/PatientCard.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import IconComponent from "../Icon/Icon";
 import styles from "./PatientCard.module.css";
 
@@ -28,30 +29,30 @@ export default function PatientCard(props: PatientCardProps) {
     }
 
     return (
-        <div className={styles.card}>
-            <div className={styles.userBox}>
-                <IconComponent icon="bx:user" className={styles.userIcon}/>
-            </div>
+        <Link href={`/patient/${props.id}`} className={styles.link}>
+                <div className={styles.userBox}>
+                    <IconComponent icon="bx:user" className={styles.userIcon}/>
+                </div>
 
-            <div className={styles.userInfo}>
-                <h2 className={styles.name}>{props.fullName}</h2>
-                <p className={styles.field}>
-                    <IconComponent icon="tabler:id-badge-2" className={styles.fieldIcon}/>
-                    {props.governmentId}
-                </p>
-                <p className={styles.field}>
-                    <IconComponent icon="mingcute:birthday-2-line" className={styles.fieldIcon}/>
-                    {`${formattedDate} (${calculateAge(parsedDate)} años)`}
-                </p>
-                <p className={styles.field}>
-                    <IconComponent icon="mingcute:phone-line" className={styles.fieldIcon}/>
-                    {props.phoneNumber ? props.phoneNumber : "Sin n. teléfono"}
-                </p>
-                <p className={styles.field}>
-                    <IconComponent icon="jam:paper-plane" className={styles.fieldIcon}/>
-                    {props.email}
-                </p>
-            </div>
-        </div>
+                <div className={styles.userInfo}>
+                    <h2 className={styles.name}>{props.fullName}</h2>
+                    <p className={styles.field}>
+                        <IconComponent icon="tabler:id-badge-2" className={styles.fieldIcon}/>
+                        {props.governmentId}
+                    </p>
+                    <p className={styles.field}>
+                        <IconComponent icon="mingcute:birthday-2-line" className={styles.fieldIcon}/>
+                        {`${formattedDate} (${calculateAge(parsedDate)} años)`}
+                    </p>
+                    <p className={styles.field}>
+                        <IconComponent icon="mingcute:phone-line" className={styles.fieldIcon}/>
+                        {props.phoneNumber ? props.phoneNumber : "Sin n. teléfono"}
+                    </p>
+                    <p className={styles.field}>
+                        <IconComponent icon="jam:paper-plane" className={styles.fieldIcon}/>
+                        {props.email}
+                    </p>
+                </div>
+        </Link>
     );
 }
