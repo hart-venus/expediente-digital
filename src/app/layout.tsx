49676414fa3d188778c57645fd7a7fcb7f2893ca
hart-venus <aleyva1509@gmail.com>
2024-03-04 07:40:00 -0600

    differenciate between login and other pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,33 @@
-import type { Metadata } from "next";
+'use client';
 import "./globals.css";
+import { LoginProvider } from "../../context/LoginContext";
+import { usePathname } from "next/navigation";
 
-export const metadata: Metadata = {
-  title: "Expediente Digital",
-  description: "Registro de pacientes y sus expedientes médicos.",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pn = usePathname();
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <LoginProvider>
+          {
+            pn === "/login" ? (
+              <>
+                {children}
+              </>
+            ) : (
+              <>
+                {children}
+              </>
+            )
+          }        
+        </LoginProvider>
+      
+      </body>
     </html>
   );
 }
